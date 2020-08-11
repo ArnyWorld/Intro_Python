c105abf9cbe315e9ff16ffac7dd053ad6bfed584
Arnaldo Muñoz <kings31lendy@gmail.com>
2020-08-11 17:32:13 -0400

    Datos de entrada

---
+++ 4.-DatosEntrada.py
@@ -0,0 +1,18 @@
+"""
+Entrada de datos por la consola de Python
+    Entrada de datos de tipo cadena
+            saludo = input()
+    Entrada de datos de tipo entero
+            numero1 = int(input())
+            numero2 = int(input())
+            print(numero1 + numero2)
+"""
+
+nombre = input("Ingrese su nombre :")
+print("Tu nombre es: ", nombre)
+
+titulo = input("Ingrese el título")
+
+autor = input("Ingrese el autor")
+
+print(titulo," fue escrito por ", autor)

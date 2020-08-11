e291d1df4fa4cf253330c573e32a776892269c3f
Arnaldo Muñoz <kings31lendy@gmail.com>
2020-08-11 17:31:26 -0400

    Manejo de variables

---
+++ 2.-variables.py
@@ -0,0 +1,19 @@
+#Manejo de variables
+x = 5
+y = 2 
+z = x + y
+print(x)
+print(y)
+print(z)
+""" para conocer la dirección en la que se encuentra una variable
+se debe ejecutar 
+                    id(x)
+                    
+Python es uno de los lenguajes mas eficientes respecto al manejo
+de la memoria RAM
+
+Si quisieramos conocer el tipo de dato se debe ejecutar
+                    type(z)
+                    
+ACLARACIÓN:  La función id y type solo se pueden ejecutar en la consola de python
+"""

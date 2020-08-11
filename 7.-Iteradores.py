e34dfa37fe4f53d281aa213d0f7442fe6d701ffb
Arnaldo Muñoz <kings31lendy@gmail.com>
2020-08-11 17:33:21 -0400

    Manejo de Iteradores

---
+++ 7.-Iteradores.py
@@ -0,0 +1,12 @@
+lenguajes = ["PHP", "GO","JAVA","KOTLIN","DART"]
+
+print(lenguajes)
+
+for lenguaje in lenguajes:
+    print(f'Estoy aprendiendo {lenguaje}')
+    
+for numero in range(0,9):
+    print(numero)
+    
+for incremento in range(0,20,3):
+    print(incremento)
